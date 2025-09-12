f693b0db00ef6b5bee063e370d6a7cdc248b0801
Md Raju Ahmed <rajuoffice220@gmail.com>
2025-09-13 02:25:25 +0600

    project create

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+  
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
